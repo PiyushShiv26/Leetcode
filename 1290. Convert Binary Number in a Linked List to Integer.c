9dfeb182f877f241ef9223f846de20b9fd40daf1
PiyushShiv26 <piyushshiv26@gmail.com>
2025-08-15 22:53:32 +0530

    Convert Binary Number in a Linked List to Integer

---
+++ 1290. Convert Binary Number in a Linked List to Integer.c
@@ -0,0 +1,56 @@
+/*
+1290. Convert Binary Number in a Linked List to Integer
+
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+Return the decimal value of the number in the linked list.
+The most significant bit is at the head of the linked list.
+
+Example 1:
+
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+
+Example 2:
+
+Input: head = [0]
+Output: 0
+ 
+Constraints:
+
+The Linked List is not empty.
+Number of nodes will not exceed 30.
+Each node's value is either 0 or 1.
+*/
+
+#include <math.h>
+#include <stdlib.h>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+int getDecimalValue(struct ListNode* head) {
+    if(head==NULL){
+        return 0;
+    }
+
+    struct ListNode* temp = head;
+
+    int k = 0;
+    while(temp!=NULL){
+        k++;
+        temp = temp->next;
+    }
+
+    int dec = 0;
+    while(head!=NULL){
+        dec += head->val*pow(2,k-1);
+        k--;
+        head = head->next;
+    }
+
+    return dec;
+}
